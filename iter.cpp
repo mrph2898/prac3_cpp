b0e8e7a0a3b3d32c4f9793e70ba100183ad7d7d2
Georgiy Kormakov <egor2898@mail.ru>
2018-03-14 00:37:29 +0300

    First try of task2

--- iter.cpp
+++ iter.cpp
@@ -1,6 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
+
+template <typename ValueType>
+class Iterable
+{
+public:
+    class InputIterator : public std::iterator<
+                              std::input_iterator_tag,
+                              ValueType,
+                              long,
+                              const ValueType*,
+                              ValueType >
+
+    {
+        ValueType value;
+    public:
+        explicit InputIterator(ValueType val) : value(val) {}
+        ValueType operator *() { return value; } //overloaded *p
+        InputIterator& operator ++() //overloaded p++
+        {
+            std::advance(*this, 1);
+            return *this;
+        }
+        InputIterator operator ++(int)
+        {
+            InputIterator tmp = *this;
+            (*this)++;
+            return tmp;
+        }
+        bool operator ==(InputIterator another) const { return value == another.value; }
+        bool operator !=(InputIterator another) const { return !( *this == another); }
+    };
+    virtual InputIterator begin() = 0;
+    virtual InputIterator end() = 0;
+
+};
+
+class Record
+{
+    std::tuple<int, int, int> time;
+    std::string message;
+public:
+    Record() : time(std::make_tuple(0, 0, 0)), message("") {}
+    Record(std::tuple<int, int, int> t, std::string mes) : time(t), message(mes) {}
+            /*std::tuple<int, int, int> t = std::make_tuple(0, 0, 0),
+            std::string mes = " ");*/
+    Record(const Record &rec) : time(rec.time), message(rec.message) {}
+        /*time = rec.time;
+        message = rec.message;
+    }*/
+};
+
+class Journal : public Iterable<Record>
+{
+    std::vector<Record> book;
+public:
+    Journal() : book() {}
+    Journal(std::vector<Record> rec) : book(rec.time, rec.message) {}
+    Journal(Journal &jour) : book(jour.book) {}
+    /*InputIterator begin()
+    {*/
+
+}
+
 int main(){
-   std::cout << "In God we trust";
+    Journal p{ {std::make_tuple(1, 2, 3), "Timmy"},
+               {std::make_tuple(12, 34, 52), "John"} };
 
 }
