edd6428a0d3bfb4a86d604e419643307939a82c7
Georgiy Kormakov <egor2898@mail.ru>
2018-03-21 00:45:51 +0300

    Forward_list is better then vector to store information

--- iter.cpp
+++ iter.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <forward_list>
 #include <vector>
 #include <iterator>
 #include <tuple>
@@ -15,35 +16,29 @@ public:
     virtual const_InputIterator end() const = 0;
 };
 
-/*typedef std::iterator<
-                      std::input_iterator_tag,
-                      std::pair<std::tuple<int, int, int>, std::string>,
-                      long,
-                      const std::pair<std::tuple<int, int, int>, std::string>*,
-                      std::pair<std::tuple<int, int, int>, std::string> > InputIterator;**/
-typedef std::vector<std::pair<std::tuple<int, int, int>, std::string>>::iterator JournalIterator;
-typedef std::vector<std::pair<std::tuple<int, int, int>, std::string>>::const_iterator const_JournalIterator;
+//I use forward_list, because it doesn't needed in bidirectional access
+typedef std::forward_list<std::pair<std::tuple<int, int, int>, std::string>>::iterator JournalIterator;
+typedef std::forward_list<std::pair<std::tuple<int, int, int>, std::string>>::const_iterator const_JournalIterator;
+typedef std::tuple<int, int, int> Time;
 
-class Journal : public Iterable<std::pair<std::tuple<int, int, int>, std::string>,
+class Journal : public Iterable<std::pair<Time, std::string>,
                                 JournalIterator,
                                 const_JournalIterator>
 {
-    std::vector<std::pair<std::tuple<int, int, int>, std::string>> book;
+    std::forward_list<std::pair<Time, std::string>> book;
 public:
     JournalIterator begin() { return book.begin();}
     JournalIterator end() { return book.end(); }
-    const_JournalIterator begin() const { return book.cbegin(); }
-    const_JournalIterator end() const { return book.cend(); }
+    const_JournalIterator begin() const { return book.begin(); } //.cbegin and .cend are equal with .begin and .end
+    const_JournalIterator end() const { return book.end(); }     //in this methods
     Journal() : book(0) {}
-    Journal(const std::pair<std::tuple<int, int, int>, std::string> &rec) { book.push_back(rec); }
-    Journal(const std::vector<std::pair<std::tuple<int, int, int>, std::string>> &b)
+    Journal(const std::pair<Time, std::string> &rec) { book.push_front(rec); }
+    Journal(const std::forward_list<std::pair<Time, std::string>> &b)
     {
         for (auto &k : b) {
-            book.push_back(k);
+            book.push_front(k);
         }
     }
- //   Journal(const Journal &jour) { book = jour.book;}
- //   friend std::ostream& operator <<(std::ostream& out, const Journal &j);
 };
 
 std::ostream& operator <<(std::ostream& out, const Journal &j){
@@ -55,20 +50,20 @@ std::ostream& operator <<(std::ostream& out, const Journal &j){
         }
         return out;
 }
-
+/*
 typedef std::vector<int>::iterator PVIterator;
 typedef std::vector<int>::iterator const_PVIterator;
 
 class PairVector : public Iterable<int, PVIterator, const_PVIterator>
 {
-    /////
+    int sequence_num;
     int N;
 public:
-    PVIterator begin();
+    PVIterator begin() { return 1; }
 };
-
+*/
 int main(){
-    Journal j ({std::make_tuple(1, 2, 3), "Timmy"});/*,
-                {std::make_tuple(2, 3, 4), "John"}});*/
+    Journal j ({std::make_tuple(1, 2, 3), "Timmy"});
+            //   {std::make_tuple(2, 3, 4), "John"});
     std::cout << j;
 }
